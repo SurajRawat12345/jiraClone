87113421260dd4d6833bd8ac2b82648d73bc5833
Suraj12345 <rawatsuraj2612@gmail.com>
2025-09-09 15:01:27 +0530

    added comment model

---
+++ Backend/models/commentModel.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const commentSchema = new mongoose.Schema({
+    taskId: { 
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: "Task", 
+        required: true 
+    },
+    userId: { 
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: "User", 
+        required: true 
+    },
+    comment: { 
+        type: String, 
+        required: true, 
+        trim: true 
+    },
+}, {
+    timestamps: true
+})
+
+export default mongoose.model("Comment", commentSchema);
